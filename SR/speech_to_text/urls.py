481de0c81564c416e4ccd57439ada01c54706e9b
Carlos0321 <max9236@naver.com>
2024-09-29 00:24:50 +0900

    Initial commit

--- SR/speech_to_text/urls.py
+++
@@ -1,7 +0,0 @@
-# speech_to_text/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('convert/', views.audio_to_text, name='convert_audio_to_text'),
-]
--- SR/text_to_speech/urls.py
+++
@@ -1,7 +0,0 @@
-# text_to_speech/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('speak/', views.text_to_audio, name='convert_text_to_audio'),
-]
